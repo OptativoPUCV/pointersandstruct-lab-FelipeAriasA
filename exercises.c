fd452e28b1d3ab4d18ee7687a1d6c13c1cea3c71
Replit user <replituser@example.com>
2021-03-23 21:15:46 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -109,7 +109,7 @@ Función que suma los vectores `a` y `b` y
 actualiza el vector `c` con el resultado de la suma.
 */
 void sumaV(Vector * a, Vector * b, Vector * c) {
-
+  c->datos[i]=a->datos[i]+b->datos[i];
 }
 
 /*
